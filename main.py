5ce5b0a67e1869ee865add29c62ac23b8a71edd7
contoh <susilorahmadi2312@gmail.com>
2023-03-17 14:24:15 +0700

    logging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import os, redis
+import logging
 
 REDIS_URL  = os.getenv("REDIS_URL", "redis.openfaas-fn.svc.cluster.local")
 REDIS      = redis.Redis(host=REDIS_URL, port=6379)
@@ -10,4 +11,5 @@ app = FastAPI()
 def read_root():
     # REDIS.set("test","Berhasil")
     # output = REDIS.get("test")
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',level=logging.INFO,datefmt='`%Y-%m-%d %H:%M:%S')
     return {"Hello": "World"}
