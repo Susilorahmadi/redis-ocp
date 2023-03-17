9d12318b52dd4080244696366399e37c65b85dd8
contoh <susilorahmadi2312@gmail.com>
2023-03-17 14:20:33 +0700

    coba

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 
 @app.get("/redis")
 def read_root():
-    REDIS.set("test","Berhasil")
-    output = REDIS.get("test")
-    return {"Hello": output}
+    # REDIS.set("test","Berhasil")
+    # output = REDIS.get("test")
+    return {"Hello": "World"}
